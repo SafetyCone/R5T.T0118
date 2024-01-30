9afeea0431c8094627733994d3f72e73f0933779
davidcoats <david@getsafetycone.com>
2024-01-29 18:34:40 -0800

    Interim changes.

--- source/R5T.T0118/Code/Attributes/ServiceTestFixtureMarkerAttribute.cs
+++ source/R5T.T0118/Code/Attributes/ServiceTestFixtureMarkerAttribute.cs
@@ -1,5 +1,7 @@
 using System;
 
+using R5T.T0143;
+
 
 namespace R5T.T0118
 {
@@ -8,6 +10,7 @@ namespace R5T.T0118
     /// Also allows specifying that a class is *not* a service test fixture. (Useful for decorating extraneous types that also happen to be in a service test fixtures directory/file.)
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    [MarkerAttributeMarker]
     public class ServiceTestFixtureMarkerAttribute : Attribute
     {
         private readonly bool zIsServiceTestFixture;
